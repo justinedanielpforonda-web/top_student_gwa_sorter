3e0a005097440f2aec03c3dd8136f2c47236fcb3
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-27 16:25:49 +0800

    added try catch error

--- gwa_highest_finder.py
+++ gwa_highest_finder.py
@@ -1,16 +1,22 @@
-#reads the file and then print it 
-with open("student_gwa.txt") as gwaStudents:
-    for line in gwaStudents:
+try:
+    #reads the file and then print it 
+    with open("student_gwa.txt") as gwaStudents:
+        line = gwaStudents.readline()
         name, gwa = line.split()
         best_name = name
-
-#conditions for gwa
-for line in gwaStudents:
-    name,gwa = line.split()
-    gwa = float(gwa)
-
-    if gwa <  bestGwa:
-        bestGwa = gwa
-        best_name = name
-
-
+        best_gwa = float(gwa)
+    #loops the each line to check if first line is lower than current line
+        for line in gwaStudents:
+            name,gwa = line.split()
+            gwa = float(gwa)
+    #conditions for gwa
+            if gwa <  best_gwa:
+                best_gwa = gwa
+                best_name = name
+    #prints result
+    print("Top student:", best_name)
+    print("Highest GWA:", best_gwa)
+except FileExistsError:
+    print("File does not exists",
+          "make sure your file is named Student_gwa.txt")
+    
