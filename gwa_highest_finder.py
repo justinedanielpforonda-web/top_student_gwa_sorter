524cc600ee27af8142855a4b20e22e449bf1aaed
justinedanielpforonda-web <justinedanielpforonda@gmail.com>
2026-04-27 15:44:13 +0800

    first value problem fixed but raises another value problem

--- gwa_highest_finder.py
+++ gwa_highest_finder.py
@@ -1,11 +1,16 @@
 #reads the file and then print it 
 with open("student_gwa.txt") as gwaStudents:
     for line in gwaStudents:
-        name, gwa = line.strip()
-#conditions for gwa
+        name, gwa = line.split()
+        best_name = name
 
+#conditions for gwa
 for line in gwaStudents:
+    name,gwa = line.split()
+    gwa = float(gwa)
+
     if gwa <  bestGwa:
         bestGwa = gwa
+        best_name = name
 
 
